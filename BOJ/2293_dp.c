f76f92315fb98fda922c8c41f7709d00edee55ab
jongseok Jang <whdtjr321@gmail.com>
2018-08-18 19:48:35 +0900

    BOJ 2293... Do not check duplication

--- BOJ/2293_dp.c
+++ BOJ/2293_dp.c
@@ -6,26 +6,38 @@ int n, k;
 
 void solve(int m, int curVal){
   int ii;
-  printf("-------m : [%d], curVal : [%d]-----\n", m, curVal);
-
+  printf("m : [%d], curVal : [%d]\n", m, curVal);
   if( m-curVal < 0 )
     return;
 
+  if( m-curVal == 0 ){
+    count[curVal]++;
+    printf("(m-curVal == 0) curVal:[%d]  count[curVal]:[%d]\n", curVal, count[curVal]);
+    return;
+  }
+    
   if( count[m-curVal] != 0){
-    count[m] += count[m-curVal];
-    printf("count : [%d], %d || [%d]: %d\n", m, count[m], m-curVal, count[m-curVal]);
+    printf("(count[m-curVal] != 0)  m: [%d](%d) m-curVal:[%d](%d)\n", m,count[m], m-curVal, count[m-curVal]);
     return;
   }
   
-  if( m-curVal == 0 ){
-
-    count[m]++;
-    printf("count : [%d], %d || [%d]: %d\n", m, count[m], m-curVal, count[m-curVal]);
-    return;
+  for( ii = 0 ; ii < n; ii++){
+    solve(m-curVal, coin[ii]);
+    if( m-curVal-coin[ii] >= 0)
+      count[m-curVal] += count[m-curVal-coin[ii]];
   }
 
-  for( ii = 0 ; ii < n; ii++)
-    solve(m-curVal, coin[ii]);
+}
+
+int solve_before(int n, int k){
+  int ii;
+  for( ii = 0; ii < n; ii++){
+    if( count[k - coin[ii]] != 0)
+      count[k] += count[ii];
+    else
+      solve(k, coin[ii]);
+  }
+  return coin[k-1];
   
 }
 
@@ -36,14 +48,10 @@ int main(void){
   for( ii = 0; ii < n; ii++)
     scanf("%d", &coin[ii]);
 
-  for( ii = 0; ii < n; ii++){
-    if( count[k - coin[ii]] != 0)
-      count[k] += count[ii];
-    else
-      solve(k, coin[ii]);
-  }
-
+  printf("%d\n",solve_before(n, k));
   for( ii = 0; ii < k; ii++)
-    printf("[%d]:[%d]\n", ii, count[ii]);
+    printf("[%d] : [%d]\n", ii, count[ii]);
+  
+
 
 }
